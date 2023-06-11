44550bccdce269937327baa8544ce765b6fe3c63
Davood Ahmad Bhat <bhatdavood21@gmail.com>
2023-06-11 23:08:30 +0530

    Updated List of Strings with Length of N

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This repository provides solutions to frequently asked Java coding questions in 
 
 
 ## Questions
-1. Implement a method that takes a list of strings and returns the List of strings that have a length greater than 5 using the Stream API.
+1. Implement a method that takes a list of strings and returns the List of strings and count of Strings that have a length greater than 5 using the Stream API.
 
 ## Usage
 
--- com/src/java/LengthOfStrings.java
+++ com/src/java/LengthOfStrings.java
@@ -8,20 +8,33 @@ public class LengthOfStrings {
     public static void main(String[] args) {
 
         /*
-        * Implement a method that takes a list of strings and returns the List of strings that have a length greater than "N" using the Stream API.
+        * Implement a method that takes a list of strings and returns
+        * (a) the List of strings that have a length greater than "N"
+        * (b) the Count of strings that have a length greater than "N"
+         * using the Stream API.
         * */
 
         List<String> fruits =
                 Arrays.asList("apple","orange","grapes","papaya","pineapple","pomegranate");
 
         System.out.println(fruitsWithNLength(fruits));
+        System.out.println("---------------------------------------");
+        System.out.println("Number of Fruits with Length greater than or equal to 7 : "+ countOfFruitsWithNLength(fruits));
 
     }
 
+    // Returns List of Fruits with length greater than or equal to 7
     public static List<String> fruitsWithNLength(List<String> fruits){
 
         return fruits.stream()
                 .filter(fruit -> fruit.length() >= 7)
                 .collect(Collectors.toList());
+    }
+
+    // Returns Count of Fruits with length greater than or equal to 7
+    public static long countOfFruitsWithNLength(List<String> fruits){
+        return fruits.stream()
+                .filter(fruit -> fruit.length() >= 7)
+                .count();
     }
 }
