a35c8c46e3c6e9e2e26404c11be0aeea73f87476
Davood Ahmad Bhat <bhatdavood21@gmail.com>
2023-06-13 21:15:15 +0530

    Updated Unique Strings in a List

--- com/src/java/model/UniqueStringsInAList.java
+++ com/src/java/model/UniqueStringsInAList.java
@@ -6,20 +6,34 @@ import java.util.List;
 public class UniqueStringsInAList {
 
     /*
-    * Implement a method that takes a list of strings and returns a new list containing only the unique strings in alphabetical order using the Stream API.
+    * Implement a method that takes a list of strings and returns a new list containing only the unique strings in
+    * (a) Ascending order
+    * (b) Descending Order
+    * using the Stream API.
     * */
 
     public static void main(String[] args) {
 
         List<String> fruits = Arrays.asList("cherry","banana","pomegranate","apple","apple");
-        System.out.println(uniqueStringsInSortedOrder(fruits));
+        System.out.println("Ascending Order : " + uniqueStringsInAscendingOrder(fruits));
+        System.out.println("Descending Order : " + uniqueStringsInDescendingOrder(fruits));
     }
 
-    public static List<String> uniqueStringsInSortedOrder(List<String> fruits){
+    public static List<String> uniqueStringsInAscendingOrder(List<String> fruits){
 
         return fruits.stream()
                     .distinct()
                     .sorted()
                     .toList();
     }
+
+    public static List<String> uniqueStringsInDescendingOrder(List<String> fruits){
+
+        return fruits.stream()
+                .distinct()
+                .sorted((s1,s2)->s2.compareTo(s1))
+                .toList();
+    }
+
+
 }
