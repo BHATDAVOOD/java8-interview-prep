fc30b9d95203dc14efa1a957e8036fdf93ce6524
Davood Ahmad Bhat <bhatdavood21@gmail.com>
2023-06-20 11:48:17 +0530

    Sum of Squares of Even Numbers

---
+++ com/src/java/SumOfSquaresOfEvenNumbers.java
@@ -0,0 +1,9 @@
+package src.java;
+
+public class SumOfSquaresOfEvenNumbers {
+
+
+    /*
+    * Implement a method that takes a list of integers and returns the sum of the squares of all the even numbers using the Stream API.
+    * */
+}
